9d2efcd5478f1433de6b298eef41be2c3d65d23b
RikkaW <rikka@shizuku.moe>
2021-03-26 12:59:04 +0800

    Add strip

--- cxx/jni/Application.mk
+++ cxx/jni/Application.mk
@@ -3,3 +3,4 @@ APP_PROJECT_PATH := $(call my-dir)/../../cxx-source
 APP_PLATFORM := android-16
 APP_CPPFLAGS := -std=c++17
 APP_STL := none
+APP_CFLAGS := -Wno-builtin-macro-redefined -D__FILE__=__FILE_NAME__
